f59eb257c9354f770399d649772dd5dd5431ece3
THANIKKUL PROMSREE <81483735+thanikkul@users.noreply.github.com>
2022-08-30 00:01:26 +0700

    Add files via upload

--- utility.js
+++ utility.js
@@ -5,8 +5,8 @@ const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/s"; // put u
 const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzkxMzYxLCJleHAiOjE2NjE3OTQ5NjEsIm5iZiI6MTY2MTc5MTM2MSwianRpIjoiTVdUbEdKUmpKdE0xZEZlbiIsInN1YiI6NzIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.yPz_QSYoFzMlJfivCM1MOJcb2o3OJX_JKhlSMUEhzJM"; // put access token
 const student = {
   id_student:63360194,
-  name: 'Kittaphat', // replace with your full name.
-  age: 19, // put your age.
+  name: 'Thanikkul', // replace with your full name.
+  age: 20, // put your age.
   gender: 'Male', // replace with your gender
   department: 'CPE'
 };
